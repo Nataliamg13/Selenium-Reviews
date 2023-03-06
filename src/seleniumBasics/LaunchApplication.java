14305423499fb7b9e976781049a241f56773ea1d
Nataliamg13 <nataliamg13@gmail.com>
2023-03-05 21:03:31 -0600

    Selenium/Review Class

---
+++ src/seleniumBasics/LaunchApplication.java
@@ -0,0 +1,8 @@
+package seleniumBasics;
+
+public class LaunchApplication {
+    public static void main(String[] args) {
+
+
+    }
+}
---
+++ src/seleniumBasics/LaunchApplications2.java
@@ -0,0 +1,12 @@
+package seleniumBasics;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+
+public class LaunchApplications2 {
+    public static void main(String[] args) {
+
+        WebDriver driver=new EdgeDriver();
+    }
+}
